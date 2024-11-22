c79212bc0df913a96e5a9ff84c44010c194d403b
CrayCrayUltraContrarian <106885248+craycrayultracontrarian@users.noreply.github.com>
2024-11-22 13:09:51 -0500

    setup

---
+++ MPD_SPVM.sql
@@ -0,0 +1,9 @@
+/*
+22 Novembre 
+Simona Bronevitsky 11316059 Yihan Hu 11330613 Jules Lemee 11303810
+Le projet consiste à créer et exécuter des requêtes sur la base de données du SPVM pour analyser 
+les données d'interventions et générer des rapports. Cela permet d'identifier des tendances, 
+d'optimiser la prise de décision, et d'améliorer la gestion des ressources.
+*/
+
+CREATE DATABASE SPVM
